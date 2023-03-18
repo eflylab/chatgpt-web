03057aacb0bf1fa84822777595578141f8928767
eflylab <eflylab@163.com>
2023-03-19 00:21:30 +0800

    feat: support admin

--- src/store/modules/auth/helper.ts
+++ src/store/modules/auth/helper.ts
@@ -1,6 +1,7 @@
 import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'SECRET_TOKEN'
+const LOCAL_ADMIN = 'SECRET_TOKEN_ADMIN'
 
 export function getToken() {
   return ss.get(LOCAL_NAME)
@@ -11,5 +12,18 @@ export function setToken(token: string) {
 }
 
 export function removeToken() {
+  removeAdmin()
   return ss.remove(LOCAL_NAME)
+}
+
+export function setAdmin(admin: boolean) {
+  return ss.set(LOCAL_ADMIN, admin)
+}
+
+export function getAdmin() {
+  return ss.get(LOCAL_ADMIN)
+}
+
+export function removeAdmin() {
+  return ss.remove(LOCAL_ADMIN)
 }
